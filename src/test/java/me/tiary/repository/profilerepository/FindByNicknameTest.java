a6127c69fa8d0b7fed3ab83305fb5c44711a325f
leverest96 <myjade0416@gmail.com>
2022-11-16 22:13:19 +0900

    [TR-23] RENAME: Rename package from repository to repository.profilerepository

--- src/test/java/me/tiary/repository/profilerepository/FindByNicknameTest.java
+++ src/test/java/me/tiary/repository/profilerepository/FindByNicknameTest.java
@@ -1,6 +1,7 @@
-package me.tiary.repository;
+package me.tiary.repository.profilerepository;
 
 import me.tiary.domain.Profile;
+import me.tiary.repository.ProfileRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
