96b738fdcc054652af1dc1b118f0e30cec717758
Igoc <zx0342@gmail.com>
2022-11-17 14:15:41 +0900

    [TR-28] FEAT: Add security exception handlers

--- src/main/java/me/tiary/config/WebSecurityConfig.java
+++ src/main/java/me/tiary/config/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package me.tiary.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.tiary.exception.handler.security.AccessDeniedExceptionHandler;
+import me.tiary.exception.handler.security.AuthenticationExceptionHandler;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,15 +10,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http,
+                                                   final AuthenticationEntryPoint authenticationEntryPoint,
+                                                   final AccessDeniedHandler accessDeniedHandler) throws Exception {
         http.authorizeRequests()
                 .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler)
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
@@ -34,5 +45,15 @@ public class WebSecurityConfig {
         return web -> web.ignoring()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
                 .requestMatchers(PathRequest.toH2Console());
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint(final ObjectMapper objectMapper) {
+        return new AuthenticationExceptionHandler(objectMapper);
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler(final ObjectMapper objectMapper) {
+        return new AccessDeniedExceptionHandler(objectMapper);
     }
 }
