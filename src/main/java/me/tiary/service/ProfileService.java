1b1eac28a40e0badf8277eef86e900e6c23ed23e
leverest96 <myjade0416@gmail.com>
2022-11-18 17:33:40 +0900

    [TR-20] FEAT: Create profile creation business logic

--- src/main/java/me/tiary/service/ProfileService.java
+++ src/main/java/me/tiary/service/ProfileService.java
@@ -1,7 +1,13 @@
 package me.tiary.service;
 
 import lombok.RequiredArgsConstructor;
+import me.tiary.domain.Profile;
+import me.tiary.dto.profile.ProfileCreationRequestDto;
+import me.tiary.dto.profile.ProfileCreationResponseDto;
+import me.tiary.exception.ProfileException;
+import me.tiary.exception.status.ProfileStatus;
 import me.tiary.repository.ProfileRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,7 +17,24 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProfileService {
     private final ProfileRepository profileRepository;
 
+    private final ModelMapper modelMapper;
+
     public boolean checkNicknameDuplication(final String nickname) {
         return profileRepository.findByNickname(nickname).isPresent();
+    }
+
+    @Transactional
+    public ProfileCreationResponseDto createProfile(final ProfileCreationRequestDto dto) {
+        final String nickname = dto.getNickname();
+
+        if (checkNicknameDuplication(nickname)) {
+            throw new ProfileException(ProfileStatus.EXISTING_NICKNAME);
+        }
+
+        final Profile profile = modelMapper.map(dto, Profile.class);
+
+        final Profile result = profileRepository.save(profile);
+
+        return modelMapper.map(result, ProfileCreationResponseDto.class);
     }
 }
