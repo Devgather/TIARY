796c552138f1b65da86a579404797cf8b7827f9c
Yuni Cho <jjoyuni@gmail.com>
2022-12-08 15:32:28 +0900

    [TR-12] FEAT: Create login business logic

--- src/main/java/me/tiary/TiaryApplication.java
+++ src/main/java/me/tiary/TiaryApplication.java
@@ -1,6 +1,7 @@
 package me.tiary;
 
 import me.tiary.properties.jwt.AccessTokenProperties;
+import me.tiary.properties.jwt.RefreshTokenProperties;
 import me.tiary.properties.security.SecurityCorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableConfigurationProperties({AccessTokenProperties.class, SecurityCorsProperties.class})
+@EnableConfigurationProperties({AccessTokenProperties.class, RefreshTokenProperties.class, SecurityCorsProperties.class})
 @EnableJpaAuditing
 public class TiaryApplication {
     public static void main(final String[] args) {
