7c1df64d414f6c6bd01b8cda8df9d4e21ce79959
Igoc <zx0342@gmail.com>
2022-12-11 19:15:44 +0900

    [TR-119] FIX: Fix Amazon S3 bean bug

--- src/main/java/me/tiary/config/AppConfig.java
+++ src/main/java/me/tiary/config/AppConfig.java
@@ -1,5 +1,10 @@
 package me.tiary.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import me.tiary.properties.aws.AwsProperties;
 import me.tiary.properties.aws.AwsS3Properties;
 import me.tiary.utility.aws.AwsS3Manager;
@@ -21,7 +26,17 @@ public class AppConfig {
     }
 
     @Bean
-    public AwsS3Manager awsS3Manager(final AwsProperties awsProperties, final AwsS3Properties awsS3Properties) {
-        return new AwsS3Manager(awsProperties, awsS3Properties);
+    public AmazonS3 amazonS3(final AwsProperties awsProperties, final AwsS3Properties awsS3Properties) {
+        final AWSCredentials credentials = new BasicAWSCredentials(awsProperties.getAccessKey(), awsProperties.getSecretKey());
+
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(awsS3Properties.getRegion())
+                .build();
+    }
+
+    @Bean
+    public AwsS3Manager awsS3Manager(final AmazonS3 amazonS3, final AwsS3Properties awsS3Properties) {
+        return new AwsS3Manager(amazonS3, awsS3Properties.getBucket());
     }
 }
