a6a70d2025afe274544dda993f434b83159bad53
Igoc <zx0342@gmail.com>
2022-12-27 15:28:16 +0900

    [TRY-51] REFACTOR: Replace email and password magic number with factory preset

--- src/test/java/common/factory/dto/account/AccountLoginRequestDtoFactory.java
+++ src/test/java/common/factory/dto/account/AccountLoginRequestDtoFactory.java
@@ -1,10 +1,11 @@
 package common.factory.dto.account;
 
+import common.config.factory.FactoryPreset;
 import me.tiary.dto.account.AccountLoginRequestDto;
 
 public final class AccountLoginRequestDtoFactory {
     public static AccountLoginRequestDto createDefaultAccountLoginRequestDto() {
-        return create("test@example.com", "test");
+        return create(FactoryPreset.EMAIL, FactoryPreset.PASSWORD);
     }
 
     public static AccountLoginRequestDto create(final String email, final String password) {
