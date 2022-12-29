226def71ca331e06dcfe92e0200cbf50bf684048
leverest96 <myjade0416@gmail.com>
2022-12-29 12:21:52 +0900

    [TRY-24] FEAT: Create profile entity picture change

---
+++ src/test/java/me/tiary/domain/profile/UpdatePictureTest.java
@@ -0,0 +1,27 @@
+package me.tiary.domain.profile;
+
+import common.config.factory.FactoryPreset;
+import common.factory.domain.ProfileFactory;
+import me.tiary.domain.Profile;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("[Profile] updatePicture")
+class UpdatePictureTest {
+    @Test
+    @DisplayName("[Success] profile picture is updated")
+    void successIfProfilePictureIsUpdated() {
+        // Given
+        final Profile profile = ProfileFactory.createDefaultProfile();
+
+        final String pictureUrl = FactoryPreset.PICTURE + "image.png";
+
+        // When
+        profile.updatePicture(pictureUrl);
+
+        // Then
+        assertThat(profile.getPicture()).isEqualTo(pictureUrl);
+    }
+}
--- src/test/java/me/tiary/service/profileservice/UploadPictureTest.java
+++ src/test/java/me/tiary/service/profileservice/UploadPictureTest.java
@@ -1,21 +1,29 @@
 package me.tiary.service.profileservice;
 
 import common.annotation.service.ServiceTest;
+import common.config.factory.FactoryPreset;
+import common.factory.domain.ProfileFactory;
+import common.factory.dto.profile.ProfilePictureUploadRequestDtoFactory;
 import common.factory.mock.web.MockMultipartFileFactory;
+import me.tiary.domain.Profile;
+import me.tiary.dto.profile.ProfilePictureUploadRequestDto;
+import me.tiary.dto.profile.ProfilePictureUploadResponseDto;
 import me.tiary.exception.ProfileException;
 import me.tiary.exception.status.ProfileStatus;
+import me.tiary.properties.aws.AwsStorageProperties;
+import me.tiary.repository.ProfileRepository;
 import me.tiary.service.ProfileService;
 import me.tiary.utility.aws.AwsS3Manager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Function;
 
@@ -27,8 +35,10 @@ import static org.mockito.Mockito.doReturn;
 @ServiceTest
 @DisplayName("[ProfileService] uploadPicture")
 class UploadPictureTest {
-    @InjectMocks
     private ProfileService profileService;
+
+    @Mock
+    private ProfileRepository profileRepository;
 
     @Mock
     private AwsS3Manager awsS3Manager;
@@ -36,23 +46,59 @@ class UploadPictureTest {
     @Spy
     private ModelMapper modelMapper;
 
+    private AwsStorageProperties awsStorageProperties;
+
     @BeforeEach
     void beforeEach() {
         modelMapper.getConfiguration()
                 .setFieldMatchingEnabled(true)
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+
+        awsStorageProperties = new AwsStorageProperties(FactoryPreset.PICTURE);
+
+        profileService = new ProfileService(
+                profileRepository,
+                awsS3Manager,
+                awsStorageProperties,
+                modelMapper
+        );
+    }
+
+    @Test
+    @DisplayName("[Fail] profile does not exist")
+    void failIfProfileDoesNotExist() {
+        // Given
+        final String profileUuid = UUID.randomUUID().toString();
+
+        doReturn(Optional.empty())
+                .when(profileRepository)
+                .findByUuid(any(String.class));
+
+        final ProfilePictureUploadRequestDto requestDto = ProfilePictureUploadRequestDtoFactory.createDefaultProfilePictureUploadRequestDto();
+
+        // When
+        final ProfileException result = assertThrows(ProfileException.class, () -> profileService.uploadPicture(profileUuid, requestDto));
+
+        // Then
+        assertThat(result.getStatus()).isEqualTo(ProfileStatus.NOT_EXISTING_PROFILE);
     }
 
     @Test
     @DisplayName("[Fail] content type is null")
     void failIfContentTypeIsNull() {
         // Given
-        final String uuid = UUID.randomUUID().toString();
+        final String profileUuid = UUID.randomUUID().toString();
+
+        doReturn(Optional.of(ProfileFactory.createDefaultProfile()))
+                .when(profileRepository)
+                .findByUuid(any(String.class));
 
         final MultipartFile multipartFile = MockMultipartFileFactory.create("image", "image.png", null, 1);
 
+        final ProfilePictureUploadRequestDto requestDto = ProfilePictureUploadRequestDtoFactory.create(multipartFile);
+
         // When
-        final ProfileException result = assertThrows(ProfileException.class, () -> profileService.uploadPicture(uuid, multipartFile));
+        final ProfileException result = assertThrows(ProfileException.class, () -> profileService.uploadPicture(profileUuid, requestDto));
 
         // Then
         assertThat(result.getStatus()).isEqualTo(ProfileStatus.NOT_EXISTING_CONTENT_TYPE);
@@ -62,38 +108,90 @@ class UploadPictureTest {
     @DisplayName("[Fail] content type is improper")
     void failIfContentTypeIsImproper() {
         // Given
-        final String uuid = UUID.randomUUID().toString();
+        final String profileUuid = UUID.randomUUID().toString();
+
+        doReturn(Optional.of(ProfileFactory.createDefaultProfile()))
+                .when(profileRepository)
+                .findByUuid(any(String.class));
 
         final MultipartFile multipartFile = MockMultipartFileFactory.createDefaultTxtMockMultipartFile(1);
 
+        final ProfilePictureUploadRequestDto requestDto = ProfilePictureUploadRequestDtoFactory.create(multipartFile);
+
         // When
-        final ProfileException result = assertThrows(ProfileException.class, () -> profileService.uploadPicture(uuid, multipartFile));
+        final ProfileException result = assertThrows(ProfileException.class, () -> profileService.uploadPicture(profileUuid, requestDto));
 
         // Then
         assertThat(result.getStatus()).isEqualTo(ProfileStatus.NOT_SUPPORTING_CONTENT_TYPE);
     }
 
     @Test
-    @DisplayName("[Success] picture is acceptable")
-    void successIfPictureIsAcceptable() {
+    @DisplayName("[Success] storage url does not end with slash")
+    void successIfStorageUrlDoesNotEndWithSlash() {
         // Given
-        final MultipartFile multipartFile = MockMultipartFileFactory.createDefaultPngMockMultipartFile(1);
+        final AwsStorageProperties awsStorageProperties = new AwsStorageProperties(FactoryPreset.PICTURE.substring(0, FactoryPreset.PICTURE.length() - 1));
+
+        final ProfileService profileService = new ProfileService(
+                profileRepository,
+                awsS3Manager,
+                awsStorageProperties,
+                modelMapper
+        );
 
         final String profileUuid = UUID.randomUUID().toString();
 
+        final Profile defaultProfile = ProfileFactory.createDefaultProfile();
+
+        doReturn(Optional.of(defaultProfile))
+                .when(profileRepository)
+                .findByUuid(any(String.class));
+
+        final ProfilePictureUploadRequestDto requestDto = ProfilePictureUploadRequestDtoFactory.createDefaultProfilePictureUploadRequestDto();
+
         final Function<String, String> titleGenerator = ProfileService.createPictureTitleGenerator(profileUuid);
 
-        final List<String> fileNameList = List.of(titleGenerator.apply(multipartFile.getOriginalFilename()));
+        final List<String> picturePathList = List.of(titleGenerator.apply(requestDto.getPictureFile().getOriginalFilename()));
 
-        doReturn(fileNameList)
+        doReturn(picturePathList)
                 .when(awsS3Manager)
                 .uploadFiles(any(), any());
 
+
         // When
-        final List<String> result = profileService.uploadPicture(profileUuid, multipartFile);
+        final ProfilePictureUploadResponseDto result = profileService.uploadPicture(profileUuid, requestDto);
 
         // Then
-        assertThat(result.size()).isEqualTo(1);
-        assertThat(result.get(0)).isEqualTo(fileNameList.get(0));
+        assertThat(result.getPicture()).isEqualTo(awsStorageProperties.getUrl() + "/" + picturePathList.get(0));
+    }
+
+    @Test
+    @DisplayName("[Success] picture is acceptable")
+    void successIfPictureIsAcceptable() {
+        // Given
+        final String profileUuid = UUID.randomUUID().toString();
+
+        final Profile defaultProfile = ProfileFactory.createDefaultProfile();
+
+        doReturn(Optional.of(defaultProfile))
+                .when(profileRepository)
+                .findByUuid(any(String.class));
+
+        final ProfilePictureUploadRequestDto requestDto = ProfilePictureUploadRequestDtoFactory.createDefaultProfilePictureUploadRequestDto();
+
+        final Function<String, String> titleGenerator = ProfileService.createPictureTitleGenerator(profileUuid);
+
+        final List<String> picturePathList = List.of(titleGenerator.apply(requestDto.getPictureFile().getOriginalFilename()));
+
+        doReturn(picturePathList)
+                .when(awsS3Manager)
+                .uploadFiles(any(), any());
+
+        final Profile updatedProfile = ProfileFactory.create(FactoryPreset.NICKNAME, awsStorageProperties.getUrl() + picturePathList.get(0));
+
+        // When
+        final ProfilePictureUploadResponseDto result = profileService.uploadPicture(profileUuid, requestDto);
+
+        // Then
+        assertThat(result.getPicture()).isEqualTo(updatedProfile.getPicture());
     }
 }
