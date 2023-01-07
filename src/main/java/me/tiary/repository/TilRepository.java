735964bcb8ec11e788c0cad693c69a76ff7a92d6
leverest96 <myjade0416@gmail.com>
2023-01-08 00:28:46 +0900

    [TRY-100] FEAT: Create TIL data access by uuid

--- src/main/java/me/tiary/repository/TilRepository.java
+++ src/main/java/me/tiary/repository/TilRepository.java
@@ -4,6 +4,9 @@ import me.tiary.domain.Til;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TilRepository extends JpaRepository<Til, Long> {
+    Optional<Til> findByUuid(final String uuid);
 }
