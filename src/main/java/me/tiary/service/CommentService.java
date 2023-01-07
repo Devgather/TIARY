b6c337778ce1a6f1642673c72bbc7969441e92ce
leverest96 <myjade0416@gmail.com>
2023-01-08 01:42:35 +0900

    [TRY-98] FEAT: Create comment writing business logic

---
+++ src/main/java/me/tiary/service/CommentService.java
@@ -0,0 +1,48 @@
+package me.tiary.service;
+
+import lombok.RequiredArgsConstructor;
+import me.tiary.domain.Comment;
+import me.tiary.domain.Profile;
+import me.tiary.domain.Til;
+import me.tiary.dto.comment.CommentWritingRequestDto;
+import me.tiary.dto.comment.CommentWritingResponseDto;
+import me.tiary.exception.CommentException;
+import me.tiary.exception.status.CommentStatus;
+import me.tiary.repository.CommentRepository;
+import me.tiary.repository.ProfileRepository;
+import me.tiary.repository.TilRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    private final ProfileRepository profileRepository;
+
+    private final TilRepository tilRepository;
+
+    private final ModelMapper modelMapper;
+
+    @Transactional
+    public CommentWritingResponseDto writeComment(final String profileUuid, final CommentWritingRequestDto requestDto) {
+        final Profile profile = profileRepository.findByUuid(profileUuid)
+                .orElseThrow(() -> new CommentException(CommentStatus.NOT_EXISTING_PROFILE));
+
+        final Til til = tilRepository.findByUuid(requestDto.getTilUuid())
+                .orElseThrow(() -> new CommentException(CommentStatus.NOT_EXISTING_TIL));
+
+        final Comment comment = Comment.builder()
+                .profile(profile)
+                .til(til)
+                .content(requestDto.getContent())
+                .build();
+
+        final Comment result = commentRepository.save(comment);
+
+        return modelMapper.map(result, CommentWritingResponseDto.class);
+    }
+}
