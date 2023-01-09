dc2c9767dd4e4c0944983dc0cb6c6498c58cd226
Yuni Cho <jjoyuni@gmail.com>
2023-01-09 21:32:42 +0900

    [TRY-103] FEAT: Create tag data access to save

---
+++ src/main/java/me/tiary/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package me.tiary.repository;
+
+import me.tiary.domain.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
