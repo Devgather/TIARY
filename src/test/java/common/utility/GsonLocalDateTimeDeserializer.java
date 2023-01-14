ded69581616b7b5f8cde02c5b16ae8e4822cb4b6
Igoc <zx0342@gmail.com>
2023-01-14 17:56:44 +0900

    [TRY-140] FEAT: Create TIL read API

---
+++ src/test/java/common/utility/GsonLocalDateTimeDeserializer.java
@@ -0,0 +1,20 @@
+package common.utility;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class GsonLocalDateTimeDeserializer implements JsonDeserializer<LocalDateTime> {
+    @Override
+    public LocalDateTime deserialize(final JsonElement json, final Type typeOfT, final JsonDeserializationContext context) throws JsonParseException {
+        return LocalDateTime.parse(
+                json.getAsString(),
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        );
+    }
+}
