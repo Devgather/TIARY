435c822eea833dd8ad20a255814e3e68b9477945
Igoc <zx0342@gmail.com>
2023-01-14 15:55:20 +0900

    [TRY-135] FEAT: Create TIL fetch join profile data access by UUID

--- src/main/java/me/tiary/repository/TilRepository.java
+++ src/main/java/me/tiary/repository/TilRepository.java
@@ -2,6 +2,8 @@ package me.tiary.repository;
 
 import me.tiary.domain.Til;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface TilRepository extends JpaRepository<Til, Long> {
     Optional<Til> findByUuid(final String uuid);
+
+    @Query("select t from Til t join fetch t.profile where t.uuid = :uuid")
+    Optional<Til> findByUuidJoinFetchProfile(@Param("uuid") final String uuid);
 }
