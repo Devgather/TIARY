1e1c9cf8ccf5d92b302c71e84b9a5711eb7c7f97
Igoc <zx0342@gmail.com>
2024-07-30 19:51:45 +0900

    [TRY-229] FEAT: Create TilTag fetch join Tag all data access by TIL UUID

--- src/main/java/me/tiary/repository/TilTagRepository.java
+++ src/main/java/me/tiary/repository/TilTagRepository.java
@@ -3,6 +3,8 @@ package me.tiary.repository;
 import me.tiary.domain.TilTag;
 import me.tiary.domain.composite.TilTagId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,9 @@ import java.util.List;
 @Repository
 public interface TilTagRepository extends JpaRepository<TilTag, TilTagId> {
     List<TilTag> findAllByTilUuid(final String tilUuid);
+
+    @Query("select tt from TilTag tt join fetch tt.tag where tt.til.uuid = :tilUuid")
+    List<TilTag> findAllByTilUuidJoinFetchTag(@Param("tilUuid") final String tilUuid);
 
     void deleteAllByTilUuid(final String tilUuid);
 }
