1c58ffb916cf8bf679f5c07916dab0876c5d164c
Igoc <zx0342@gmail.com>
2024-09-06 20:56:56 +0900

    [TRY-239] FEAT: Create TilTag fetch join TIL pageable data access by TIL profile nickname and tag name

--- src/main/java/me/tiary/repository/TilTagRepository.java
+++ src/main/java/me/tiary/repository/TilTagRepository.java
@@ -2,6 +2,8 @@ package me.tiary.repository;
 
 import me.tiary.domain.TilTag;
 import me.tiary.domain.composite.TilTagId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +13,10 @@ import java.util.List;
 
 @Repository
 public interface TilTagRepository extends JpaRepository<TilTag, TilTagId> {
+    @Query(value = "select tt from TilTag tt join fetch tt.til where tt.til.profile.nickname = :tilProfileNickname and tt.tag.name = :tagName",
+            countQuery = "select count(tt) from TilTag tt where tt.til.profile.nickname = :tilProfileNickname and tt.tag.name = :tagName")
+    Page<TilTag> findByTilProfileNicknameAndTagNameJoinFetchTil(@Param("tilProfileNickname") final String tilProfileNickname, @Param("tagName") final String tagName, final Pageable pageable);
+
     List<TilTag> findAllByTilUuid(final String tilUuid);
 
     @Query("select tt from TilTag tt join fetch tt.tag where tt.til.uuid = :tilUuid")
