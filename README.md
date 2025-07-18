1c03a3eb49e6d763e2e853fd174764daf3bbaaea
Igoc <zx0342@gmail.com>
2025-07-18 22:22:39 +0900

    BUILD: Initialize project with 2.0.0

--- README.md
+++ README.md
@@ -17,26 +17,6 @@
 
 </div>
 
-## 시작하기
-
-애플리케이션이 정상적으로 동작하려면 추가적인 설정이 필요하며, 관련 내용은 [Configuration](https://github.com/Devgather/TIARY/wiki/Configuration) 문서를 참고하세요. 다음 명령어를 실행하면 설정된 프로파일에 따라 애플리케이션이 실행됩니다:
-
-``` sh
-./gradlew bootRun --args='--spring.profiles.active=<profile>'
-```
-
-> [!TIP]
-> local 프로파일의 경우 브라우저에서 [http://localhost:8080/h2-console](http://localhost:8080/h2-console)에 접속하면 데이터베이스를 확인할 수 있습니다.
-
-| 설정 항목 | 값 |
-| - | - |
-| Driver Class | org.h2.Driver |
-| JDBC URL | jdbc:h2:mem:tiary;MODE=MYSQL |
-| User Name | sa |
-| Password | |
-
-더 자세한 사용 방법은 [Wiki](https://github.com/Devgather/TIARY/wiki)에서 확인하실 수 있습니다.
-
 [github-contributors-badge]: https://img.shields.io/github/contributors/Devgather/TIARY?color=green&labelColor=black&style=flat-square
 [github-contributors-link]: https://github.com/Devgather/TIARY/graphs/contributors
 [github-forks-badge]: https://img.shields.io/github/forks/Devgather/TIARY?color=blue&labelColor=black&style=flat-square
--- src/main/java/me/tiary/TiaryApplication.java
+++ src/main/java/me/tiary/TiaryApplication.java
@@ -1,32 +1,13 @@
 package me.tiary;
 
-import me.tiary.properties.aws.AwsProperties;
-import me.tiary.properties.aws.AwsS3Properties;
-import me.tiary.properties.aws.AwsStorageProperties;
-import me.tiary.properties.jwt.AccessTokenProperties;
-import me.tiary.properties.jwt.RefreshTokenProperties;
-import me.tiary.properties.mail.MailProperties;
-import me.tiary.properties.redis.RedisProperties;
-import me.tiary.properties.security.SecurityCorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-        AccessTokenProperties.class,
-        RefreshTokenProperties.class,
-        SecurityCorsProperties.class,
-        AwsProperties.class,
-        AwsS3Properties.class,
-        AwsStorageProperties.class,
-        MailProperties.class,
-        RedisProperties.class
-})
-@EnableScheduling
 public class TiaryApplication {
+
     public static void main(final String[] args) {
         SpringApplication.run(TiaryApplication.class, args);
     }
+
 }
